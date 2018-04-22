b60fd65b48ef610bb785be4d4ca75e4da9d9cbdc
Jannik <jannik.prinz@gmail.com>
2018-04-22 21:16:00 +0200

    [Feature] Import Crypto++ library and implement simple test method for AES

--- WordFortress/WordFortress.cpp
+++ WordFortress/WordFortress.cpp
@@ -1,6 +1,45 @@
 #include "WordFortress.h"
+#ifdef _DEBUG
+#  pragma comment ( lib, "cryptlibd" )
+#else
+#  pragma comment ( lib, "cryptlib" )
+#endif
 
 int main()
 {
+	AutoSeededRandomPool rnd;
+
+	SecByteBlock key(0x00, 32);
+	rnd.GenerateBlock(key, key.size());
+
+	// Generate a random IV
+	SecByteBlock iv(AES::BLOCKSIZE);
+	rnd.GenerateBlock(iv, iv.size());
+
+	byte plainText[] = "Hello! How are you.";
+	size_t messageLen = std::strlen((char*)plainText) + 1;
+
+	//////////////////////////////////////////////////////////////////////////
+	// Encrypt
+
+	std::cout << "Text: " << plainText << std::endl;
+	std::cout << "Key: " << key << std::endl;
+	std::cout << "IV: " << iv << std::endl;
+
+	CFB_Mode<AES>::Encryption cfbEncryption(key, key.size(), iv);
+	cfbEncryption.ProcessData(plainText, plainText, messageLen);
+
+	std::cout << "Encrypted Text: " << plainText << std::endl;
+
+	//////////////////////////////////////////////////////////////////////////
+	// Decrypt
+
+	CFB_Mode<AES>::Decryption cfbDecryption(key, key.size(), iv);
+	cfbDecryption.ProcessData(plainText, plainText, messageLen);
+
+	std::cout << "Decrypted Text: " << plainText << std::endl;
+
+	int i = 0;
+	std::cin >> i;
 	return 0;
 }
--- WordFortress/WordFortress.h
+++ WordFortress/WordFortress.h
@@ -1,4 +1,11 @@
 #ifndef LOGIC_WORDFORTRESS_H
 #define LOGIC_WORDFORTRESS_H
 
+#include <iostream>
+#include "..\..\cryptopp\aes.h"
+#include "..\..\cryptopp\modes.h"
+#include "..\..\cryptopp\osrng.h"
+
+using namespace CryptoPP;
+
 #endif // !LOGIC_WORDFORTRESS_H
