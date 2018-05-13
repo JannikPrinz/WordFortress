83407200326df1ed1571af4a4adcc6d775168c6c
Jannik <jannik.prinz@gmail.com>
2018-05-13 11:30:54 +0200

    [Feature] Add option table to database file. Exclude database files from repo

--- WordFortress/Constants.h
+++ WordFortress/Constants.h
@@ -7,6 +7,29 @@
 // ======================= SQL statements: ==========================
 // ******************************************************************
 
-#define CREATE_NEW_DATABASE "PRAGMA foreign_keys = ON; CREATE TABLE IF NOT EXISTS MailAccounts (MailID INTEGER PRIMARY KEY, MailAddress TEXT); CREATE TABLE IF NOT EXISTS Passwords (PasswordID INTEGER PRIMARY KEY, Salt TEXT, Password TEXT); CREATE TABLE IF NOT EXISTS Entries (EnrtryID INTEGER PRIMARY KEY, Service TEXT, UserName TEXT, MailID INTEGER REFERENCES MailAccounts (MailID), Notes TEXT, PasswordID INTEGER REFERENCES Passwords(PasswordID));"
+/*
+ * MailAccounts
+ *	- MailID INTEGER PRIMARY KEY
+ *	- MailAddress TEXT
+ *
+ * Passwords
+ *	- PasswordID INTEGER PRIMARY KEY
+ *	- Salt TEXT
+ *	- Password TEXT
+ *
+ * Entries
+ *	- EntryID INTEGER PRIMARY KEY
+ *	- Serivce TEXT
+ *	- UserName TEXT
+ *	- MailID INTEGER REFERENCES MailAccounts (MailID)
+ *	- Notes TEXT
+ *	- PasswordID INTEGER REFERENCES Passwords(PasswordID)
+ *
+ * Options
+ *	- OptionID INTEGER PRIMARY KEY
+ *	- OptionKey TEXT
+ *	- OptionValue TEXT
+ */
+#define CREATE_NEW_DATABASE "PRAGMA foreign_keys = ON; CREATE TABLE IF NOT EXISTS MailAccounts (MailID INTEGER PRIMARY KEY, MailAddress TEXT); CREATE TABLE IF NOT EXISTS Passwords (PasswordID INTEGER PRIMARY KEY, Salt TEXT, Password TEXT); CREATE TABLE IF NOT EXISTS Entries (EnrtryID INTEGER PRIMARY KEY, Service TEXT, UserName TEXT, MailID INTEGER REFERENCES MailAccounts (MailID), Notes TEXT, PasswordID INTEGER REFERENCES Passwords(PasswordID)); CREATE TABLE IF NOT EXISTS Options(OptionID INTEGER PRIMARY KEY, OptionKey TEXT, OptionValue TEXT);"
 
 #endif RESSOURCE_CONSTANTS_H
