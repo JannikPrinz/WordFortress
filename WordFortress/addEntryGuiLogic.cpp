8dafc73ad765a8f6de495544fb7fc77a2ee2c203
Jannik <jannik.prinz@gmail.com>
2018-05-20 22:36:07 +0200

    [Feature] Add sanity checks for the input fields of the AddEntryGui

--- WordFortress/addEntryGuiLogic.cpp
+++ WordFortress/addEntryGuiLogic.cpp
@@ -15,5 +15,24 @@ AddEntryGuiLogic::~AddEntryGuiLogic()
 
 void AddEntryGuiLogic::AddEntry(WordFortressAddEntryGui* gui)
 {
-
+	if (gui->GetKey() == "")
+	{
+		wxMessageBox(wxString(_("Please enter a key.")), wxString(_("Key needed")), wxICON_EXCLAMATION);
+		return;
+	}
+	if (gui->GetService() == "")
+	{
+		wxMessageBox(wxString(_("Please enter an identifiert for the service.")), wxString(_("Service needed")), wxICON_EXCLAMATION);
+		return;
+	}
+	if (gui->GetPassword() == "")
+	{
+		wxMessageBox(wxString(_("Please enter a password.")), wxString(_("Password needed")), wxICON_EXCLAMATION);
+		return;
+	}
+	if (gui->GetKey() != gui->GetKeyRepeat())
+	{
+		wxMessageBox(wxString(_("The content of the two key fields have to be the same.")), wxString(_("Keys must match")), wxICON_EXCLAMATION);
+		return;
+	}
 }
